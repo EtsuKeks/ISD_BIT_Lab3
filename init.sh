bcadbac455a0eb4dee951ff8b05af49e4e03c509
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2023-12-12 01:29:25 +0300

    Another commit

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo docker create -t karimullin_jar_holder busybox
-sudo docker build -t karimullin_build_container -f Dockerfile_jar_builder .
-sudo docker run --rm -v "$(pwd)/jar_storage":/ISD_BIT_Lab3 -d karimullin_build_container
+sudo docker build -t karimullin_builder -f Dockerfile_builder .
+sudo docker run --rm -v "$(pwd)/jar_holder":/app -d karimullin_builder
 sudo docker build -t karimullin_ansible_image -f Dockerfile_ansible .
 sudo docker-compose -p 'karimullin_proj' up
