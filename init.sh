c6f194b9671c29ab89ab64d4a8c6e62a4fa9a5fe
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2023-12-12 02:12:04 +0300

    Another commit

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo docker build -t karimullin_builder -f Dockerfile_builder .
-sudo docker run --rm -v "$(pwd)/jar_holder":/app -d karimullin_builder
+sudo docker build -t karimullin_builder_image -f Dockerfile_builder .
+sudo docker create -it --name karimullin_builder karimullin_builder_image bash
+sudo docker cp karimullin_builder:/app ./app
+sudo docker rm -f karimullin_builder
 sudo docker build -t karimullin_ansible_image -f Dockerfile_ansible .
 sudo docker-compose -p 'karimullin_proj' up
